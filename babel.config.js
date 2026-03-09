6a92c34c4d862087fcb3198e9d686d5a54ed0f46
EmmanuelJYokoyama <emmanuelnoronha2@gmail.com>
2026-03-09 19:23:52 -0300

    feature: started files for development

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@modules':    './src/modules',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@hooks':      './src/hooks',
+          '@services':   './src/services',
+          '@store':      './src/store',
+          '@utils':      './src/utils',
+          '@types':      './src/types',
+          '@assets':     './src/assets',
+        },
+      },
+    ],
+  ],
 };
